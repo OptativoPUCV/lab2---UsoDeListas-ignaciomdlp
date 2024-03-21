7d496022abe1b1d8624e05a5de26ea3b7cb85799
Your Name <you@example.com>
2024-03-21 01:25:23 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,7 +43,7 @@ Al finalizar retorna la lista creada.
 List *crea_lista() {
   List *L = create_list();
   for (int i = 1; i <= 10; i++) {
-    pushBack(L, &i);
+    pushFront(L, &i);
   }
   return L;
 }
